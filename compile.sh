411825d833fad0983d5926c8afba83e8449d78bb
Andreas Willich <sabotageandi@gmail.com>
2011-11-16 23:00:03 +0100

    add libTUIO.jar to output

--- compile.sh
+++ compile.sh
@@ -3,4 +3,5 @@ javac -Xlint:unchecked -O -source 1.5 -target 1.5 -cp lib/libTUIO.jar TuioMouse.
 jar cfm bin/TuioMouse.jar mouseManifest.inc TuioMouse.class
 javac -Xlint:unchecked -O -source 1.5 -target 1.5 -cp lib/libTUIO.jar TuioTouchpad.java
 jar cfm bin/TuioTouchpad.jar touchpadManifest.inc TuioTouchpad.class
+cp lib/libTUIO.jar bin/
 rm -f *.class
