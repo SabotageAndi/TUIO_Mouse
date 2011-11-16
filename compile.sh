80b40190fcdaa0c9e46e3101dc5330b70fcf3f4a
Andreas Willich <sabotageandi@gmail.com>
2011-11-16 22:56:24 +0100

    add libs and adapt compile.sh

--- compile.sh
+++ compile.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
-javac -Xlint:unchecked -O -source 1.5 -target 1.5 -cp ../libTUIO.jar TuioMouse.java
-jar cfm ../TuioMouse.jar mouseManifest.inc TuioMouse.class
-javac -Xlint:unchecked -O -source 1.5 -target 1.5 -cp ../libTUIO.jar TuioTouchpad.java
-jar cfm ../TuioTouchpad.jar touchpadManifest.inc TuioTouchpad.class
+javac -Xlint:unchecked -O -source 1.5 -target 1.5 -cp lib/libTUIO.jar TuioMouse.java
+jar cfm bin/TuioMouse.jar mouseManifest.inc TuioMouse.class
+javac -Xlint:unchecked -O -source 1.5 -target 1.5 -cp lib/libTUIO.jar TuioTouchpad.java
+jar cfm bin/TuioTouchpad.jar touchpadManifest.inc TuioTouchpad.class
 rm -f *.class
